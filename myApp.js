adddff0e24720e756351a798daf9feaaa3971d0c
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-08-03 04:30:10 +0700

    done

--- myApp.js
+++ myApp.js
@@ -5,11 +5,12 @@
 
 var express = require('express'); // Do Not Edit
 var app = express();              // Do Not Edit
+var helmet = require('helmet');
 
 // ----
 
 /** - Challenges - *
-********************/ 
+********************/
 
 /** 1) Install and require `helmet` */
 
@@ -30,7 +31,7 @@ var app = express();              // Do Not Edit
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
 // Use `helmet.hidePoweredBy()``
-
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
@@ -45,8 +46,8 @@ var app = express();              // Do Not Edit
 
 // We don't need our app to be framed, so you should use `helmet.frameguard()`
 // passing to it the configuration object `{action: 'deny'}`
+app.use(helmet.frameguard({ action: 'deny' }));
 
- 
 
 /** 4) Mitigate the risk of XSS - `helmet.xssFilter()` */
 
@@ -68,7 +69,7 @@ var app = express();              // Do Not Edit
 // It still has limited support.
 
 // Use `helmet.xssFilter()`
-
+app.use(helmet.xssFilter());
 
 
 /** 5) Avoid inferring the response MIME type - `helmet.noSniff()` */
@@ -81,7 +82,7 @@ var app = express();              // Do Not Edit
 // instructing the browser to not bypass the provided `Content-Type`.
 
 // Use `helmet.noSniff()`
-
+app.use(helmet.noSniff());
 
 
 /** 6) Prevent IE from opening *untrusted* HTML - `helmet.ieNoOpen()` */
@@ -94,12 +95,12 @@ var app = express();              // Do Not Edit
 // to prevent IE users from executing downloads in the *trusted* site's context.
 
 // Use `helmet.ieNoOpen()`
-
+app.use(helmet.ieNoOpen());
 
 
 /**  7) Ask browsers to access your site via HTTPS only - `helmet.hsts()` */
 
-// HTTP Strict Transport Security (HSTS) is a web security policy mechanism which 
+// HTTP Strict Transport Security (HSTS) is a web security policy mechanism which
 // helps to protect websites against protocol downgrade attacks and cookie hijacking.
 // If your website can be accessed via HTTPS you can ask user's browsers
 // to avoid using insecure HTTP. Setting the header `Strict-Transport-Security`
@@ -108,12 +109,17 @@ var app = express();              // Do Not Edit
 // the initial request.
 
 // Configure `helmet.hsts()` to instruct browsers to use HTTPS for the next
-// **90 days**, passing the config object {maxAge: timeInMilliseconds}. 
-// HyperDev already has **hsts** enabled, to override its settings you need to 
-// set the field `force` to `true` in the config object. To not alter hyperdev security 
+// **90 days**, passing the config object {maxAge: timeInMilliseconds}.
+// HyperDev already has **hsts** enabled, to override its settings you need to
+// set the field `force` to `true` in the config object. To not alter hyperdev security
 // policy we will intercept and restore the header, after inspecting it for testing.
 
 var ninetyDaysInMilliseconds = 90*24*60*60*1000;
+
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInMilliseconds,
+  force: true
+}))
 
 
 //**Note**:
@@ -132,7 +138,7 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 // DNS prefetching, at the cost of a performance penalty.
 
 // Use `helmet.dnsPrefetchControl()`
-
+app.use(helmet.dnsPrefetchControl());
 
 
 /** 9) Disable Client-Side Caching - `helmet.noCache()` */
@@ -144,7 +150,7 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 // use this option only when there is a real need.
 
 // Use helmet.noCache()
-
+app.use(helmet.noCache());
 
 
 /** 10) Content Security Policy - `helmet.contentSecurityPolicy()` */
@@ -171,20 +177,25 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 // Set also the `scriptSrc` directive so that you will allow scripts to be downloaded
 // from your website, and from the domain `trusted-cdn.com`.
 //
-// **Hint**: 
-// in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
+// **Hint**:
+// in the `"'self'"` keyword, the single quotes are part of the keyword itself,
 // so it needs to be enclosed in **double quotes** to be working.
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
 
 
-
-/** TIP: */ 
+/** TIP: */
 
 // `app.use(helmet())` will automatically include all the middleware
 // presented above, except `noCache()`, and `contentSecurityPolicy()`,
-// but these can be enabled if necessary. You can also disable or 
+// but these can be enabled if necessary. You can also disable or
 // set any other middleware individually, using a configuration object.
 
-// // - Example - 
+// // - Example -
 // app.use(helmet({
 //   frameguard: {              // configure
 //     action: 'deny'
